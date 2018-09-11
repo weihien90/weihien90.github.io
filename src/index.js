2444c05bae03a86b4804f5dc0596c3091d5a2a47
weihien90 <weihien90@gmail.com>
2018-09-11 12:46:42 +0800

    Move App to components folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import App from './components/App';
 
 import 'startbootstrap-freelancer/css/freelancer.min.css';
 
